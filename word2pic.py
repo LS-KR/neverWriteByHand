6f4dc36f1c6a39775dec692a88e3de505992700b
Elihuso Quigley <Elihuso@outlook.com>
2023-12-13 09:14:25 +0800

    [+] Dark blue ink

--- word2pic.py
+++ word2pic.py
@@ -19,9 +19,9 @@ def isAlphaBet(s=''):
     return True
 
 
-def word2pic(txt_path='./source.txt', ttf_path="./src/writeup.TTF", save_path="./result/", size=4, white=False):
-    font = ImageFont.truetype(ttf_path, 25)  # 设置字体
-    f = open(txt_path, 'r', encoding='utf-8')  # 设置文档
+def word2pic(txt_path='./source.txt', ttf_path="./src/writeup.TTF", save_path="./result/", size=4, white=False, fill=(0, 0, 0, 255)):
+    font = ImageFont.truetype(ttf_path, 25)  # Setup Font
+    f = open(txt_path, 'r', encoding='utf-8')  # Setup Text
     string = f.read()
     f.close()
     lenstr = len(string)
@@ -38,7 +38,7 @@ def word2pic(txt_path='./source.txt', ttf_path="./src/writeup.TTF", save_path=".
                 if string[flag] == '\n':
                     flag += 1
                     break
-                draw.text((random.random() * size / 2 + j, 83 + random.random() * size + i * 48), string[flag], (0, 0, 0), font=font)
+                draw.text((random.random() * size / 2 + j, 83 + random.random() * size + i * 48), string[flag], fill, font=font)
                 if isAlphaBet(string[flag]):
                     j += 13
                 else:
@@ -54,10 +54,11 @@ if __name__ == "__main__":
         for file in files:
             if file.endswith('.png'):
                 os.remove(root + '/' + file)
-    size = 2  # 混乱度
-    txt_path = './source.txt'  # 文档位置
-    ttf_path = "src/writeup.TTF"  # 字体位置
-    save_path = "./result/"  # 储存文件夹 若没有不会自动生成
-    white = False    # 若为 True 则生成白底
-    word2pic(txt_path, ttf_path, save_path, size, white)
+    size = 2  # Chaos
+    txt_path = './source.txt'  # Text File
+    ttf_path = "src/writeup.TTF"  # Font
+    save_path = "./result/"  # storage folder
+    white = False    # If True, a white background is generated
+    fill = (0, 0, 96, 255)   # Color (RGBA)
+    word2pic(txt_path, ttf_path, save_path, size, white, fill)
     print("success!")
